234d2c99ba122e6b90d95836d745952704e1e569
Felippe Fernandes <felippefsantana02@gmail.com>
2021-03-16 19:01:43 -0300

    first commit

---
+++ frontend/src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlusSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faPlusSquare, faTrash)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
